07ec3dbb4b8964ef5498a777894ba8e896558458
ivanugs <ivansegundo702@gmail.com>
2023-09-02 18:05:16 -0600

    Cambios

--- script.js
+++ script.js
@@ -1,19 +1,17 @@
-// Función para generar una matriz de cartas
+//funcion para generar el array de cartas
 function generateCards() {
-    const cardValues = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H'];
+    const values = ['A', 'B', 'C', 'D', 'E', 'F'];
     const cards = [];
-    
-    // Duplicar cada valor de carta para que haya pares
+    //duplicar cada valor para que haya pares
     for (let i = 0; i < 2; i++) {
-        for (let j = 0; j < cardValues.length; j++) {
-            cards.push(cardValues[j]);
+        for (let j = 0; j < values.length; j++) {
+            cards.push(values[j]);
         }
     }
-    
     return cards;
 }
 
-// Función para mezclar el array de cartas
+//funcion para mezclar el array de cartas
 function shuffleCards(cards) {
     for (let i = cards.length - 1; i > 0; i--) {
         const j = Math.floor(Math.random() * (i + 1));
@@ -21,41 +19,41 @@ function shuffleCards(cards) {
     }
 }
 
-// Función para crear el tablero del juego
-function createGameBoard(cards) {
-    const gameBoard = document.getElementById('game-board');
-    
-    // Mezclar las cartas
+//crear el div de tablero de juego
+function createTablero(cards) {
+    const tablero = document.getElementById('row');
+    tablero.classList.add('row')
+
+    // Revolver las cartas
     shuffleCards(cards);
-    
-    // Crear elementos div para representar las cartas
+
+    // Crear los divs para mostrar las cartas
     cards.forEach(function(card, index) {
         const cardDiv = document.createElement('div');
         cardDiv.className = 'card';
-        cardDiv.dataset.value = card; // Almacenar el valor de la carta en un atributo de datos
-        cardDiv.dataset.index = index; // Almacenar el índice de la carta en un atributo de datos
-        cardDiv.textContent = '?'; // Mostrar un signo de interrogación en la carta
-        
-        // Agregar un manejador de eventos para hacer clic en la carta
+        cardDiv.classList.add('col-3')
+        cardDiv.dataset.value = card; // Almacenar el valor de la carta
+        cardDiv.dataset.index = index; // Almacenar el índice de la carta
+
+        // Crear la imagen de la carta
+        const img = document.createElement("img");
+        img.src = 'img/question.jpg';
+
         cardDiv.addEventListener('click', flipCard);
-        
-        gameBoard.appendChild(cardDiv);
+        cardDiv.appendChild(img);
+        tablero.appendChild(cardDiv);
     });
 }
 
-// Función para voltear una carta
+
+//funcion para voltear una carta
 function flipCard(event) {
     const cardDiv = event.target;
     const cardValue = cardDiv.dataset.value;
     const cardIndex = cardDiv.dataset.index;
     
-    cardDiv.textContent = cardValue; // Mostrar el valor de la carta
-    
-    // Aquí puedes agregar lógica para verificar si dos cartas coinciden
-    // y realizar otras acciones del juego
-    
-    // Ejemplo: setTimeout(function() { cardDiv.textContent = '?'; }, 1000);
+    cardDiv.textContent = cardValue; //mostrar el valor de la carta
 }
 
 const cards = generateCards();
-createGameBoard(cards);
+createTablero(cards);
